0d15e59bb3b5b6dbc0830fb61c7c2afde31e10f9
jriemenschneider <jriemenschneider@luc.edu>
2020-11-12 00:10:30 -0500

    Working on setting up user authentication and created our firebase database

---
+++ www/script.js
@@ -0,0 +1,72 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAMAJd3prCsJjPkR6xcSiv5EB7jSFLLu48",
+    authDomain: "comp322-jmkek.firebaseapp.com",
+    databaseURL: "https://comp322-jmkek.firebaseio.com",
+    projectId: "comp322-jmkek",
+    storageBucket: "comp322-jmkek.appspot.com",
+    messagingSenderId: "904116987452",
+    appId: "1:904116987452:web:cb9975b4e0a2ac2fd54539"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//Don't know below stuff to....
+function playAudio() {
+    //initial url relative to WWW directory - then built for Android
+    var $audioURL = buildURL("media/audio/egypt.mp3");
+    var $audio = new Media($audioURL, null, errorReport);
+    $audio.play();
+    alert("playing audio...have fun!");
+    }
+    //build url for android
+function buildURL(file) {
+    if (device.platform.toLowerCase() === "android") {
+    var $androidFile = "/android_asset/www/" + file;
+    return $androidFile;
+    }
+    }
+    //return any error message from media playback
+function errorReport(error) {
+    alert("Error with Audio - " + JSON.stringify(error));
+    }
+
+
+//here(all below are user authentication functions)
+    function newUser(){
+        email = document.getElementById("createEmail").value;
+        password = document.getElementById("createPassword").value;
+        name = document.getElementById("createName").value;
+    
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
+            var user = firebase.auth().currentUser;
+            
+            userref.child(user.uid).set({
+                // "user": user.value,
+                "fullname": name,
+                "saveditems": null,
+                "email": email
+                // "cartitems": cart.value
+            });
+        }, function(error) {
+        // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
+    
+}
+function logIn(){
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+    });
+}
+
+function signOut(){
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+    }).catch(function(error) {
+        // An error happened.
+    });
+}
