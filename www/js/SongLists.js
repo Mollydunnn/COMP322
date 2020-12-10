055cc2779218f8a0078cec748598c58e949d94ed
jriemenschneider <jriemenschneider@luc.edu>
2020-12-10 06:06:50 -0500

    Finally finished the playlist and current song page funcitons to connect to genre select

--- www/js/SongLists.js
+++ www/js/SongLists.js
@@ -1,7 +1,7 @@
 //Function to generate list based on number of songs present
 //Passes in an array of each song, the length is used to generate list items
 
-function createSongList(array) {
+/*function createSongList(array) {
     var songList = document.createElement('ul');
     for (var i=0; i<array.length; i++) {
         var item = document.createElement('li');
@@ -9,4 +9,4 @@ function createSongList(array) {
         songList.appendChild(item);
     }
     return songList;
-}
+}*/
